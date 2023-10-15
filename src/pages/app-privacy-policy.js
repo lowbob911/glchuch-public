203a9e24cc2b8262ef60af0eab975710a2dda268
Sergey Sherby <sergey.sherby@sportsfusion.co.uk>
2023-10-15 19:12:36 +0200

    song app privacy policy data page

---
+++ src/pages/app-privacy-policy.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Link} from "gatsby";
+
+export default function AppPrivacyPolicy(){
+    return (
+        <div className="row text-center">
+            <div className="col-12">
+                <h1>Privacy Policy</h1>
+                <p>Приложение не собирает персональны данные / Application doesn't collect personal data</p>
+            </div>
+        </div>
+    )
+}
